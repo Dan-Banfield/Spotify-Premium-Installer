f1a33ef22a25d1c4cede6e8f4c0b49614275cf41
Dan Banfield <dandbanfield@gmail.com>
2024-11-04 11:27:54 +0000

    Add project files.

---
+++ Spotify Premium Installer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Spotify_Premium_Installer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
